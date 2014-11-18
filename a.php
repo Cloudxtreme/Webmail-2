e63e6e97c570cf666203424fa43f0b2ab6517b21
prape <zhaoshikun@foxmail.com>
2014-11-18 18:30:51 +0800

    mime reason and easyui and test.html

---
+++ a.php
@@ -0,0 +1,96 @@
+parts:object(stdClass)#3 (11) { 
+	["type"]=> int(1) 
+	["encoding"]=> int(0) 
+	["ifsubtype"]=> int(1) 
+	["subtype"]=> string(5) "MIXED" 
+	["ifdescription"]=> int(0) 
+	["ifid"]=> int(0) 
+	["ifdisposition"]=> int(0) 
+	["ifdparameters"]=> int(0) 
+	["ifparameters"]=> int(1) 
+	["parameters"]=> array(1) { 
+		[0]=> object(stdClass)#4 (2) { 
+			["attribute"]=> string(8) "BOUNDARY" 
+			["value"]=> string(41) "----=_NextPart_546ACAC3_090A5758_28E97C90" 
+		} 
+	} 
+	["parts"]=> array(2) { 
+		[0]=> object(stdClass)#5 (11) { 
+			["type"]=> int(1) 
+			["encoding"]=> int(0) 
+			["ifsubtype"]=> int(1) 
+			["subtype"]=> string(11) "ALTERNATIVE" 
+			["ifdescription"]=> int(0) 
+			["ifid"]=> int(0) 
+			["ifdisposition"]=> int(0) 
+			["ifdparameters"]=> int(0) 
+			["ifparameters"]=> int(1) 
+			["parameters"]=> array(1) { 
+				[0]=> object(stdClass)#6 (2) { 
+					["attribute"]=> string(8) "BOUNDARY" 
+					["value"]=> string(41) "----=_NextPart_546ACAC3_090A5758_7D84DC72" 
+				} 
+			} 
+			["parts"]=> array(2) { 
+				[0]=> object(stdClass)#7 (12) { 
+					["type"]=> int(0) 
+					["encoding"]=> int(3) 
+					["ifsubtype"]=> int(1) 
+					["subtype"]=> string(5) "PLAIN" 
+					["ifdescription"]=> int(0) 
+					["ifid"]=> int(0) 
+					["lines"]=> int(2) 
+					["bytes"]=> int(72) 
+					["ifdisposition"]=> int(0) 
+					["ifdparameters"]=> int(0) 
+					["ifparameters"]=> int(1) 
+					["parameters"]=> array(1) { 
+						[0]=> object(stdClass)#8 (2) { 
+							["attribute"]=> string(7) "CHARSET" 
+							["value"]=> string(5) "utf-8" } 
+					} 
+				} 
+				[1]=> object(stdClass)#9 (12) { 
+							["type"]=> int(0) 
+							["encoding"]=> int(3) 
+							["ifsubtype"]=> int(1) 
+							["subtype"]=> string(4) "HTML" 
+							["ifdescription"]=> int(0) 
+							["ifid"]=> int(0) 
+							["lines"]=> int(3) 
+							["bytes"]=> int(90) 
+							["ifdisposition"]=> int(0) 
+							["ifdparameters"]=> int(0) 
+							["ifparameters"]=> int(1) 
+							["parameters"]=> array(1) { 
+								[0]=> object(stdClass)#10 (2) { 
+									["attribute"]=> string(7) "CHARSET" 
+									["value"]=> string(5) "utf-8" 
+								} 
+							} 
+				} 
+			} 
+		} 
+		[1]=> object(stdClass)#11 (11) { 
+			["type"]=> int(3) 
+			["encoding"]=> int(3) 
+			["ifsubtype"]=> int(1) 
+			["subtype"]=> string(12) "OCTET-STREAM" 
+			["ifdescription"]=> int(0) 
+			["ifid"]=> int(0) 
+			["bytes"]=> int(14408) 
+			["ifdisposition"]=> int(0) 
+			["ifdparameters"]=> int(0) 
+			["ifparameters"]=> int(1) 
+			["parameters"]=> array(2) { 
+				[0]=> object(stdClass)#12 (2) {
+					["attribute"]=> string(7) "CHARSET" 
+					["value"]=> string(5) "utf-8" } 
+				[1]=> object(stdClass)#13 (2) { 
+					["attribute"]=> string(4) "NAME" 
+					["value"]=> string(9) "ewew.xlsx" 
+				} 
+			} 
+		} 
+	} 
+}
--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 //open imap stream
+date_default_timezone_set('Asia/Chongqing');
 $url="{imap.qq.com:993/imap/ssl}";
 $mail=imap_open($url,'774590565','zsk2aixgp');
 //get folder list
@@ -23,9 +24,9 @@ foreach($drafts as $draft){
 	echo "recent:".iconv("GB2312","UTF-8",imap_mime_header_decode($draft->recent)[0]->text)."<br/>";
 }
 //get reivew of INBOX box
-imap_reopen($mail,$url."INBOX");
+imap_reopen($mail,$url."Drafts");
 echo "<br/>there are ". imap_num_msg($mail) . " INBOX";
-$drafts=imap_fetch_overview($mail,"1:4");
+$drafts=imap_fetch_overview($mail,"1:2");
 foreach($drafts as $draft){
 	echo "<br/>".str_repeat("=",80)."<br/>";
 	echo "subject:".iconv("GB2312","UTF-8",imap_mime_header_decode($draft->subject)[0]->text)."<br/>";
@@ -38,22 +39,46 @@ foreach($drafts as $draft){
 	echo "recent:".iconv("GB2312","UTF-8",imap_mime_header_decode($draft->recent)[0]->text)."<br/>";
 }
 //get structure of INBOX
-//messagenum:1 multi-part
-$structure=imap_fetchstructure($mail,1);
+//messagenum:1 multi-part,tranfer encoding is 0
+$structure=imap_fetchstructure($mail,'2');
 echo "<br/>".str_repeat("=",80)."<br/>";
 echo "primary body type:".$structure->type."<br/>";
 echo "body transfer encoding:".$structure->encoding."<br/>";
 echo "parts:";
-var_dump($structure->parts);
-$body=imap_fetchbody($mail,1,1);
-echo iconv('GB2312','UTF-8',$body);
-//messagenum:4 it's text type,and encode is base64
-$structure=imap_fetchstructure($mail,4);
+var_dump($structure);
+foreach($structure->parts as $key => $value){
+	$t_encoding=$structure->parts[$key]->encoding;
+	//$name=$structure->parts[$key]->dparameters[0]->value;
+	$message=imap_fetchbody($mail,'2',$key+1);
+	$encoding=$structure->parts[$key]->parameters[0]->value;
+	echo "<br/>===================".$encoding."-------------------<br/>";
+	echo "part".$key."<br/>";
+	if($t_encoding ==0 or $t_encoding ==1){
+		$message=imap_8bit($message);
+	}
+	else if($t_encoding==2){
+		$message=imap_binary($message);
+	}
+	else if($t_encoding==3){
+		$message=imap_base64($message);
+	}
+	else if($t_encoding==4){
+		$message=quoted_printable_decode($message);
+	}
+	if($encoding=='gb2312'){
+		echo iconv("GB2312","UTF-8",$message);
+	}
+	else if($encoding=='utf-8'){
+		echo $message;
+	}
+}
+//messagenum:4 it's text type,and transfer encoding is base64
+$structure=imap_fetchstructure($mail,2);
 echo "<br/>".str_repeat("=",80)."<br/>";
 echo "primary body type:".$structure->type."<br/>";
 echo "body transfer encoding:".$structure->encoding."<br/>";
 //get body's content when body's type is text(0)
-$body=imap_fetchbody($mail,4,1);
+$body=imap_fetchbody($mail,2,'1');
 $f=fopen('test.txt','w');
 fwrite($f,imap_base64($body));
 fclose($f);
